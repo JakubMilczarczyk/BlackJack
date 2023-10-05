f88cefe8b0d0941b305e7b115f2a452c25409878
dev global <milczarczykjakub@gmail.com>
2023-10-05 22:42:04 +0200

    docstrings added

--- deck.py
+++ deck.py
@@ -1,20 +1,46 @@
-"""In this module you see a class named deck"""
+"""
+In this module you see a class named deck
+"""
 
-from card import Card
 from random import shuffle
+from card import Card
 
 
 class Deck:
+    """
+    class Deck makes deck from objects named Card,
+    can shuffle list of cards and hit one card each time you need
+    """
     def __init__(self):
+        """
+        The __init__ function is called when the object is created.
+        It initializes the cards list with all possible combinations of colors and values.
+
+        :param self: Represent the instance of the object itself
+        :return: The list of cards
+        """
         self.cards = []
         for color in Card.possible_colors:
             for value in Card.possible_values:
                 self.cards.append(Card(color=color, value=value))
 
     def shuffle(self):
+        """
+        The shuffle function takes the deck of cards and shuffles them.
+        It does this by using the shuffle function from random,
+        which is imported at the top of this file.
+
+        :param self: Refer to the object itself
+        :return: Nothing
+        """
         shuffle(self.cards)
 
     def hit(self):
+        """
+        The hit function will return a card from the deck.
+            It uses pop() to take the last card off of the deck and return it.
+
+        :param self: Keep track of specific instances of a class
+        :return: The last card in the deck
+        """
         return self.cards.pop()
-
-
--- deck_test.py
+++ deck_test.py
@@ -1,12 +1,30 @@
+"""Tests for Deck class"""
+
 from deck import Deck
 
 
 def test_creation():
+
+    """
+    The test_creation function tests the creation of a deck.
+    It does this by creating a new deck and checking that it has 52 cards.
+
+    :return: The number of cards in the deck
+    """
     deck = Deck()
     assert len(deck.cards) == 52
 
 
 def test_shuffle_cards():
+
+    """
+    The test_shuffle_cards function tests the shuffle method of the Deck class.
+    It does this by creating a new deck, shuffling it,
+    and then comparing it to an unshuffled version of itself.
+    If they are not equal, then we know that the shuffle method worked.
+
+    :return: A boolean value
+    """
     deck = Deck()
     shuffled_deck = deck.cards[:]
     deck.shuffle()
@@ -14,6 +32,16 @@ def test_shuffle_cards():
 
 
 def test_check_hit_card():
+
+    """
+    The test_check_hit_card function tests the hit method of the Deck class.
+    The test_check_hit_card function creates a deck object and then assigns
+    last card in the deck to a variable called last card. Then it calls
+    the hit method on that deck object and assigns it to another variable called card.
+    Finally, it asserts that those two variables are equal.
+
+    :return: The last card in the deck
+    """
     deck = Deck()
     last_card = deck.cards[-1]
     card = deck.hit()
@@ -21,6 +49,14 @@ def test_check_hit_card():
 
 
 def test_hit_card():
+    """
+    The test_hit_card function tests the hit method of the Deck class.
+    It does this by creating a new deck, calling the hit method on it, and then checking that:
+        1) The card returned is no longer in the deck (i.e., it was removed from self.cards).
+        2) The length of self.cards decreased by one.
+
+    :return: The card that was removed from the deck
+    """
     deck = Deck()
     card = deck.hit()
     assert card not in deck.cards
